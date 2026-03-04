ed84667e45c380f21b78309f93bae39fde932056
senoptick <113616549+senoptick@users.noreply.github.com>
2026-03-04 18:10:21 +0300

    Update TambovScreenCtrl.py

--- TambovScreenCtrl.py
+++ TambovScreenCtrl.py
@@ -11,31 +11,8 @@ VIDEO_FILE = "video.mp4"
 # Процесс mpv
 current_process = None
 
-def run_command(command):
-    global current_process
-    # Если процесс уже запущен — убиваем его
-    if current_process:
-        current_process.terminate()
-    # Запускаем новую команду
-    current_process = subprocess.Popen(command, shell=True, env={"DISPLAY": ":0"})
-
-def on_press(key):
-    global is_black
-    print("asd")
-    try:
-        if key == keyboard.Key.f12:  # замените на нужную клавишу
-            if is_black:
-                run_command(f"mpv --vo=x11 --fullscreen --image-display-duration=inf {BLACK_IMAGE}")
-            else:
-                run_command(f"mpv --vo=x11 --fullscreen --loop-file=inf {VIDEO_FILE}")
-            is_black = not is_black
-    except Exception as e:
-        print(f"Error: {e}")
-
 def main():
-    # Ловим нажатия клавиш
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
-
+   run_command(f"mpv --vo=x11 --fullscreen --image-display-duration=inf {BLACK_IMAGE}")
+    
 if __name__ == "__main__":
     main()
