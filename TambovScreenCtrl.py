dd41df3ea99faf3e3c7e324aeab5e4407d79e4ab
senoptick <113616549+senoptick@users.noreply.github.com>
2026-03-04 18:12:36 +0300

    Update TambovScreenCtrl.py

--- TambovScreenCtrl.py
+++ TambovScreenCtrl.py
@@ -10,6 +10,13 @@ VIDEO_FILE = "video.mp4"
 
 # Процесс mpv
 current_process = None
+def run_command(command):
+    global current_process
+    # Если процесс уже запущен — убиваем его
+    if current_process:
+        current_process.terminate()
+    # Запускаем новую команду
+    current_process = subprocess.Popen(command, shell=True, env={"DISPLAY": ":0"})
 
 def main():
    run_command(f"mpv --vo=x11 --fullscreen --image-display-duration=inf {BLACK_IMAGE}")
